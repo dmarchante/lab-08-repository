4456804fb704545030693cc58c0e238e2d869d6e
David Marchante <cdmarchante@gmail.com>
2019-05-02 09:38:46 -0700

    update schema to include drop table

--- schema.sql
+++ schema.sql
@@ -1,4 +1,10 @@
-CREATE TABLE location (
+DROP TABLE locations IF EXISTS;
+
+DROP TABLE weather IF EXISTS;
+
+DROP TABLE events IF EXISTS;
+
+CREATE TABLE locations (
   search_query VARCHAR (255),
   formatted_query VARCHAR(255),
   latitude DECIMAL,
@@ -7,12 +13,12 @@ CREATE TABLE location (
 
 CREATE TABLE weather (
   forcast VARCHAR(255),
-  time DATE,
+  time_of_day DATE,
 );
 
 CREATE TABLE events (
   link VARCHAR (255),
-  name VARCHAR(255),
+  event_name VARCHAR(255),
   event_date DATE,
   summary VARCHAR (255),
 );
